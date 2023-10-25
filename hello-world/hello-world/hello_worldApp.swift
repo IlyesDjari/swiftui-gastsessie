fd79633b9517fc40c3e43186dca05c37bd5b3021
IlyesDjari <ilyes.djari@student.ehb.be>
2023-10-25 14:13:53 +0200

    1. Hello World

---
+++ hello-world/hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Ilyes Djari on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
